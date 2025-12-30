27af898e3b5c1cdb07eef58b30ac7246278f8944
Mati <180681mvs@gmail.com>
2025-12-30 12:35:01 -0300

    1er commit

---
+++ content.js
@@ -0,0 +1,9 @@
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".hero h1").innerText =
+    "Masajes relajantes premium en Córdoba";
+
+  document.querySelector(".hero img").src =
+    "/assets/hero-masajes-real.jpg";
+});
+</script>
